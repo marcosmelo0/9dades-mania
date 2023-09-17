541643a9a372963bb5eef78f0a55d271a16eb561
Marcos Melo <marcosmelo012@gmail.com>
2023-09-16 23:52:14 -0300

    feat: create shared button

--- src/modules/login/screens/login.tsx
+++ src/modules/login/screens/login.tsx
@@ -1,8 +1,13 @@
 import { Text, View } from "react-native";
 import { ContainerLogin } from "../styles/login.styles";
-import { Input } from "../../shared/components/input/input";
+import Input  from "../../shared/components/input/Input";
+import Button from "../../shared/components/button/Button";
 
 const Login = () => {
+    const handleButton = () => {
+        console.log("clicou")
+    }
+
     return (
         <View>
             <ContainerLogin>
@@ -10,6 +15,7 @@ const Login = () => {
                     Olá
                 </Text>
                 <Input />
+                <Button margin="16px 0" title="ENTRAR" onPress={handleButton}/>
             </ContainerLogin>
         </View>
     )
--- src/modules/shared/components/input/input.tsx
+++ src/modules/shared/components/input/input.tsx
@@ -4,8 +4,9 @@ import { ContainerInput } from "./input.style"
 
 type InputProps = TextInputProps;
 
-export const Input = ({...props}: InputProps) => {
+const Input = ({...props}: InputProps) => {
     return (
         <ContainerInput {...props}/>
     )
 }
+export default Input;
